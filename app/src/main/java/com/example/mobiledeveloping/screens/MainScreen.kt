2d48529e10d7702be3db5f6090c2ac2512ef3636
Taxic27 <Danila_Taxic@mail.ru>
2024-11-12 04:44:08 +0300

    video 3

--- app/src/main/java/com/example/mobiledeveloping/screens/MainScreen.kt
+++ app/src/main/java/com/example/mobiledeveloping/screens/MainScreen.kt
@@ -1,6 +1,7 @@
 package com.example.mobiledeveloping.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,6 +11,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,10 +22,13 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.mobiledeveloping.R
+import com.example.mobiledeveloping.ui.theme.BlueLight
 
 @Preview(showBackground = true)
 @Composable
@@ -39,7 +46,7 @@ fun MainScreen (){
     ) {
         Card(
             modifier = Modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(10.dp)
+            colors = CardDefaults.cardColors(containerColor = BlueLight)
         ){
             Column(
                 modifier = Modifier.fillMaxWidth(),
@@ -50,16 +57,66 @@ fun MainScreen (){
                     horizontalArrangement = Arrangement.SpaceBetween
                 ){
                     Text(
-                        modifier = Modifier.padding(top = 8.dp, start = 8.dp),
                         text = "12 November 2024 11:00",
+                        modifier = Modifier.padding(
+                            top = 8.dp,
+                            start = 8.dp
+                        ),
                         color = Color.White
                     )
                     AsyncImage(
                         model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
                         contentDescription = "im2",
-                        modifier = Modifier.size(35.dp)
-                            .padding(top = 3.dp, end = 8.dp)
+                        modifier = Modifier
+                            .padding(
+                                top = 3.dp,
+                                end = 8.dp
+                            )
+                            .size(35.dp)
                     )
+                }
+                Text(
+                    text = "Kolomna",
+                    style = TextStyle(fontSize = 24.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "10°C",
+                    style = TextStyle(fontSize = 65.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "Sunny",
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ){
+                    IconButton(
+                        onClick = {
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            contentDescription = "im3"
+                        )
+                    }
+                    Text(
+                        text = "23°C/12°C",
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+                    IconButton(
+                        onClick = {
+                        }
+                    ){
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_sync),
+                            contentDescription = "im4"
+                        )
+                    }
                 }
             }
         }
